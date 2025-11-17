f53b563ee31f32ff34644f03d78895e15cf3a780
MrMarzeero <otaviorp2014@gmail.com>
2025-11-17 10:49:26 -0300

    added CORS

--- factory.py
+++ factory.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from config import Config
 
+
+cors = CORS(support_credentials=True)
 db = SQLAlchemy()
 
 def create_app():
