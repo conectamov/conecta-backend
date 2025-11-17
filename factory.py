d88bd51ec277049b1ae55189873b07e9f087e02c
MrMarzeero <otaviorp2014@gmail.com>
2025-11-17 10:51:37 -0300

    added spectree

--- factory.py
+++ factory.py
@@ -2,10 +2,17 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from config import Config
-
+from spectree import SpecTree
 
 cors = CORS(support_credentials=True)
 db = SQLAlchemy()
+api = SpecTree(
+    'flask',
+    title="API-Conecta",
+    path="docs",
+    version="v1.0",
+    security=[{"BearerAuth": []}]
+)
 
 def create_app():
     app = Flask(__name__)
