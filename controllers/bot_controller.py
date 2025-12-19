654a827d76f72b4d8aa3c9b350dee45e85689ca3
MrMarzeero <otaviorp2014@gmail.com>
2025-12-18 23:55:35 -0300

    feat: added bot_controller.py

--- controllers/bot_controller.py
+++ controllers/bot_controller.py
@@ -1,7 +1,13 @@
-import requests
 from flask import Blueprint, request
-from config import Config 
-from factory import api
+from flask_jwt_extended import jwt_required
+from models.bot import UserAnswer, MatchQuestion
+
+bot_blueprint = Blueprint("bot-blueprint", __name__, url_prefix="/bot")
+
+
+@bot_blueprint.get("/")
+def get_all():
+    return {"success": True}
 
 bot_blueprint = Blueprint('bot-blueprint', __name__, url_prefix="/bot")
 
